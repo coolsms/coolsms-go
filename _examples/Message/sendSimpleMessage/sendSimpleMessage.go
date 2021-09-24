901e09186d46ba004645da6be11b8537d2e1fc84
wileyday <wiley@nurigo.net>
2021-09-24 09:04:41 +0900

    indent 수정(tab --> space 2)

--- _examples/Message/sendSimpleMessage/sendSimpleMessage.go
+++ _examples/Message/sendSimpleMessage/sendSimpleMessage.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
 
-	"github.com/coolsms/coolsms-go"
+  "github.com/coolsms/coolsms-go"
 )
 
 func main() {
-	client := coolsms.NewClient()
+  client := coolsms.NewClient()
 
-	// 메시지 데이터
-	message := make(map[string]interface{})
-	message["to"] = "01000000000"
-	message["from"] = "029302266"
-	message["text"] = "안녕하세요 홍길동님 회원가입을 환영합니다."
-	message["type"] = "SMS"
+  // 메시지 데이터
+  message := make(map[string]interface{})
+  message["to"] = "01000000000"
+  message["from"] = "029302266"
+  message["text"] = "안녕하세요 홍길동님 회원가입을 환영합니다."
+  message["type"] = "SMS"
 
-	// 포토문자(MMS)
-	// Storage 예제에서 이미지 파일 업로드 방법을 확인하실 수 있습니다.
-	// message["type"] = "MMS"
-	// message["imageId"] = "ST01FJ20073019363R8G58YLK1kvUXEH"
-	// message["subject"] = "Subject Title"
+  // 포토문자(MMS)
+  // Storage 예제에서 이미지 파일 업로드 방법을 확인하실 수 있습니다.
+  // message["type"] = "MMS"
+  // message["imageId"] = "ST01FJ20073019363R8G58YLK1kvUXEH"
+  // message["subject"] = "Subject Title"
 
-	// 장문문자(LMS)
-	// message["type"] = "LMS"
-	// message["subject"] = "Subject Title"
+  // 장문문자(LMS)
+  // message["type"] = "LMS"
+  // message["subject"] = "Subject Title"
 
-	// 친구톡(CTA)
-	// 사이트에서 플러스친구 연동 후 사용이 가능합니다.
-	// message["type"] = "CTA"
-	// kakaoOptions := make(map[string]interface{})
-	// kakaoOptions["pfId"] = "KA01PF2003231823W4979UIEbV3fHtkY"
-	// message["kakaoOptions"] = kakaoOptions
+  // 친구톡(CTA)
+  // 사이트에서 플러스친구 연동 후 사용이 가능합니다.
+  // message["type"] = "CTA"
+  // kakaoOptions := make(map[string]interface{})
+  // kakaoOptions["pfId"] = "KA01PF2003231823W4979UIEbV3fHtkY"
+  // message["kakaoOptions"] = kakaoOptions
 
-	// 알림톡(CTA)
-	// 사이트에서 플러스친구 연동 및 템플릿 등록 후 사용이 가능합니다.
-	// message["type"] = "ATA"
-	// kakaoOptions := make(map[string]interface{})
-	// kakaoOptions["pfId"] = "KA01PF2003231823W4979UIEbV3fHtkY"
-	// kakaoOptions["templateId"] = "KA01TP2003W3882345595hMWrRtFCSTq"
-	// message["kakaoOptions"] = kakaoOptions
+  // 알림톡(CTA)
+  // 사이트에서 플러스친구 연동 및 템플릿 등록 후 사용이 가능합니다.
+  // message["type"] = "ATA"
+  // kakaoOptions := make(map[string]interface{})
+  // kakaoOptions["pfId"] = "KA01PF2003231823W4979UIEbV3fHtkY"
+  // kakaoOptions["templateId"] = "KA01TP2003W3882345595hMWrRtFCSTq"
+  // message["kakaoOptions"] = kakaoOptions
 
-	params := make(map[string]interface{})
-	params["message"] = message
+  params := make(map[string]interface{})
+  params["message"] = message
 
   // 컨피그 임의로 설정
   // client.Messages.Config = map[string]string{
@@ -50,13 +50,13 @@ func main() {
   //   "APISecret": "IXHBDCUIKZKUEDPL3KQFQNOIJ15ZHKVL",
   // }
 
-	// API 호출 후 결과값을 받아 옵니다.
-	result, err := client.Messages.SendSimpleMessage(params)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+  // API 호출 후 결과값을 받아 옵니다.
+  result, err := client.Messages.SendSimpleMessage(params)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
-	// Print Result
-	fmt.Printf("%+v\n", result)
+  // Print Result
+  fmt.Printf("%+v\n", result)
 }
